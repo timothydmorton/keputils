659bffe36958a8c4b1639131a8be84c313f8f58b
Timothy Morton <tim.morton@gmail.com>
2014-05-02 09:48:30 -0400

    now puts data files in ~/.keputils

--- getdata.sh
+++ getdata.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-wget 'http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&select=*' -O kois_cumulative.csv
-wget 'http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=keplerstellar&select=*' -O keplerstellar.csv
+mkdir -p ~/.keputils
+
+wget 'http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&select=*' -O ~/.keputils/kois_cumulative.csv
+wget 'http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=keplerstellar&select=*' -O ~/.keputils/keplerstellar.csv
 
