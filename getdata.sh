975aefc0608fd7883be4141f059310010926f708
Timothy Morton <tim.morton@gmail.com>
2014-08-06 16:44:00 -0400

    added LICENSE

--- getdata.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-mkdir -p ~/.keputils
-
-wget 'http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=cumulative&select=*' -O ~/.keputils/kois_cumulative.csv
-wget 'http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/nph-nstedAPI?table=keplerstellar&select=*' -O ~/.keputils/keplerstellar.csv
-
