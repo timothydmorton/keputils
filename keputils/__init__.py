43be8c7b4fdb1d2015ff4de446371261cf465ae3
Timothy Morton <tim.morton@gmail.com>
2016-04-19 15:06:30 -0400

    updated setup.py, versioning

--- keputils/__init__.py
+++ keputils/__init__.py
@@ -1,1 +1,9 @@
-__all__ = ['koiutils','kicutils']
+__version__ = '0.2'
+
+try:
+    __KEPUTILS_SETUP__
+except NameError:
+    __KEPUTILS_SETUP__ = False
+
+if not __KEPUTILS_SETUP__:
+	__all__ = ['koiutils','kicutils']
--- setup.py
+++ setup.py
@@ -1,11 +1,35 @@
 from setuptools import setup
 
+# Hackishly inject a constant into builtins to enable importing of the
+# package before the library is built.
+import sys
+if sys.version_info[0] < 3:
+    import __builtin__ as builtins
+else:
+    import builtins
+builtins.__KEPUTILS_SETUP__ = True
+import keputils
+version = keputils.__version__
+
+
+# Publish the library to PyPI.
+if "publish" in sys.argv[-1]:
+    os.system("python setup.py sdist upload")
+    sys.exit()
+
+# Push a new tag to GitHub.
+if "tag" in sys.argv:
+    os.system("git tag -a {0} -m 'version {0}'".format(version))
+    os.system("git push --tags")
+    sys.exit()
+
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
 setup(name = "keputils",
-    version = "0.2",
+    version = version,
     description = "Basic module for interaction with KOI and Kepler-stellar tables.",
     long_description = readme(),
     author = "Timothy D. Morton",
