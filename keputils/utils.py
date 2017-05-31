dd2c6b2a1a0ffce012fe1acefbaae7c3fef545da
Timothy Morton <tdm@astro.princeton.edu>
2017-05-30 23:00:14 -0400

    added get_catalog for real

--- keputils/utils.py
+++ keputils/utils.py
@@ -1,6 +1,29 @@
 import numpy as np
 import re
 
+#### from DFM #####
+import requests
+from cStringIO import StringIO
+
+def get_catalog(name, basepath=KEPUTILS):
+    fn = os.path.join(basepath, "{0}.h5".format(name))
+    if os.path.exists(fn):
+        return pd.read_hdf(fn, name)
+    if not os.path.exists(basepath):
+        os.makedirs(basepath)
+    print("Downloading {0}...".format(name))
+    url = ("http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/"
+           "nph-nstedAPI?table={0}&select=*").format(name)
+    r = requests.get(url)
+    if r.status_code != requests.codes.ok:
+        r.raise_for_status()
+    fh = StringIO(r.content)
+    df = pd.read_csv(fh)
+    df.to_hdf(fn, name, format="t")
+    return df
+
+######
+
 def koiname(k, star=False, koinum=False):
     """Returns KOI name in standard format.
 
